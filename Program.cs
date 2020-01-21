8dbad75d2ef119bd2fad13a3396e07c5ec23caa2
Brennan Harrison <brennan16@usf.edu>
2020-01-21 17:51:24 -0500

    Change 1.000

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace My_First_Git_Solution
     {
         static void Main(string[] args)
         {
+            // This is a comment.
             Console.WriteLine("Hello World!");
+
         }
     }
 }
